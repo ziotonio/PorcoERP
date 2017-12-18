d1f1057945867074d86c469aed40dfe05151d89f
f.troccoli <f.troccoli@c81b5bb8-e782-4fe7-967c-ac36ce986653>
2017-12-18 15:44:43 +0000

    refs closes #9420 @2h00m

--- src/cn/itcast/invoice/auth/emp/dao/impl/EmpDaoImpl.java
+++ src/cn/itcast/invoice/auth/emp/dao/impl/EmpDaoImpl.java
@@ -14,49 +14,67 @@ import cn.itcast.invoice.util.base.BaseDaoImpl;
 import cn.itcast.invoice.util.base.BaseQueryModel;
 
 public class EmpDaoImpl extends BaseDaoImpl<EmpModel> implements EmpDao{
-	public void doQbc(DetachedCriteria dc,BaseQueryModel qm){
-		EmpQueryModel eqm = (EmpQueryModel) qm;
+	
+	public void addUserName(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getUserName()!=null && eqm.getUserName().trim().length()>0){
 			dc.add(Restrictions.like("userName", "%"+eqm.getUserName().trim()+"%"));
 		}
+	}
+	
+	public void addName(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getName()!=null && eqm.getName().trim().length()>0){
 			dc.add(Restrictions.like("name", "%"+eqm.getName().trim()+"%"));
 		}
+	}
+	
+	public void addTele(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getTele()!=null && eqm.getTele().trim().length()>0){
 			dc.add(Restrictions.like("tele", "%"+eqm.getTele().trim()+"%"));
 		}
+	}
+	
+	public void addGender(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getGender()!=null && eqm.getGender()!=-1){
 			dc.add(Restrictions.eq("gender", eqm.getGender()));
 		}
+	}
+	
+	public void addEmail(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getEmail()!=null && eqm.getEmail().trim().length()>0){
 			dc.add(Restrictions.like("email", "%"+eqm.getEmail().trim()+"%"));
 		}
+	}
+	
+	public void addLastLoginTime(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getLastLoginTime()!=null){
 			dc.add(Restrictions.ge("lastLoginTime", eqm.getLastLoginTime()));
 		}
+	}
+	
+	public void addLastLoginTime2(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getLastLoginTime2()!=null){
 			dc.add(Restrictions.le("lastLoginTime", eqm.getLastLoginTime2()+86400000L));
 		}
+	}
+	
+	public void addDm(DetachedCriteria dc,EmpQueryModel eqm) {
 		if(eqm.getDm()!=null && eqm.getDm().getUuid()!=null && eqm.getDm().getUuid()!=-1){
-			//dc.add(Restrictions.eq("dm", eqm.getDm()));
 			dc.createAlias("dm", "d");
 			dc.add(Restrictions.eq("d.uuid", eqm.getDm().getUuid()));
-			
-			/*
-			am.bm.cm.dm.uuid
-			
-			dc.createAlias("am", "a");
-			dc.createAlias("a.bm", "b");
-			dc.createAlias("b.cm", "c");
-			dc.createAlias("c.dm", "d");
-			dc.add(Restrictions.eq("d.uuid", eqm.getDm().getUuid()));
-			
-			dc.createAlias("am", "a");
-			dc.createAlias("a.bm", "b");
-			dc.createAlias("b.cm", "c");
-			dc.add(Restrictions.eq("c.dm", eqm.getDm()));
-			*/
 		}
+	}	
+	
+	public void doQbc(DetachedCriteria dc,BaseQueryModel qm){
+		EmpQueryModel eqm = (EmpQueryModel) qm;
+		
+		addUserName(dc,eqm);
+		addName(dc,eqm);
+		addTele(dc,eqm);
+		addEmail(dc,eqm);
+		addLastLoginTime(dc,eqm);
+		addLastLoginTime2(dc,eqm);
+		addDm(dc,eqm);
+	
 	}
 	
 	public static void main(String[] args) {
@@ -68,9 +86,9 @@ public class EmpDaoImpl extends BaseDaoImpl<EmpModel> implements EmpDao{
 		1406246400000 
 		1406246400000+86400000
 		8:00:00 - 7:59:59
-		0-8上班
-		12月13 -12月13日
-		12月14日0点到7:59:59
+		0-8ä¸Šç�­
+		12æœˆ13 -12æœˆ13æ—¥
+		12æœˆ14æ—¥0ç‚¹åˆ°7:59:59
 		*/
 	}
 	
@@ -82,7 +100,7 @@ public class EmpDaoImpl extends BaseDaoImpl<EmpModel> implements EmpDao{
 	}
 
 	public boolean updatePwdByUserNameAndPwd(String userName, String oldPwd, String newPwd) {
-		//执行update操作 	update
+		//æ‰§è¡Œupdateæ“�ä½œ 	update
 		String hql = "update EmpModel set pwd = ? where userName = ? and pwd = ?";
 		return this.getHibernateTemplate().bulkUpdate(hql,newPwd,userName,oldPwd)>0; 
 	}
