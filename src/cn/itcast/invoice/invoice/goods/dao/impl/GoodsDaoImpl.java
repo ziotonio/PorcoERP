10f9a2cc3ff5064cb1a61adec3f46ca6ade1dd1f
f.schiavone13 <f.schiavone13@c81b5bb8-e782-4fe7-967c-ac36ce986653>
2017-12-19 10:23:22 +0000

    refs closes #9419 @2h00m

--- src/cn/itcast/invoice/invoice/goods/dao/impl/GoodsDaoImpl.java
+++ src/cn/itcast/invoice/invoice/goods/dao/impl/GoodsDaoImpl.java
@@ -15,9 +15,14 @@ import cn.itcast.invoice.util.base.BaseDaoImpl;
 import cn.itcast.invoice.util.base.BaseQueryModel;
 
 public class GoodsDaoImpl extends BaseDaoImpl<GoodsModel> implements GoodsDao{
-	public void doQbc(DetachedCriteria dc,BaseQueryModel qm){
-		GoodsQueryModel gqm = (GoodsQueryModel) qm;
-		
+	
+	/**
+	 * Adding Gtm to dc
+	 * 
+	 * @param dc
+	 * @param qm
+	 */
+	public static void addGtm(GoodsQueryModel gqm, DetachedCriteria dc){
 		if(gqm.getGtm()!=null && gqm.getGtm().getSm()!=null && gqm.getGtm().getSm().getUuid()!=null && gqm.getGtm().getSm().getUuid()!=-1){
 			/*
 			dc.createAlias("gtm", "gt");
@@ -27,28 +32,99 @@ public class GoodsDaoImpl extends BaseDaoImpl<GoodsModel> implements GoodsDao{
 			dc.createAlias("gtm", "gt");
 			dc.add(Restrictions.eq("gt.sm", gqm.getGtm().getSm()));
 		}
-		
+	}
+	
+	/**
+	 * Adding name to dc
+	 * @param gqm
+	 * @param dc
+	 */
+	public static void addName(GoodsQueryModel gqm, DetachedCriteria dc){
 		if(gqm.getName()!=null && gqm.getName().trim().length()>0){
 			dc.add(Restrictions.like("name", "%"+gqm.getName().trim()+"%"));
 		}
+	}
+	
+	/**
+	 * Adding producer to dc
+	 * @param gqm
+	 * @param dc
+	 */
+	public static void addProducer(GoodsQueryModel gqm, DetachedCriteria dc) {
 		if(gqm.getProducer()!=null && gqm.getProducer().trim().length()>0){
 			dc.add(Restrictions.like("producer", "%"+gqm.getProducer().trim()+"%"));
 		}
+	}
+
+	/**
+	 * Adding unit to dc
+	 * @param gqm
+	 * @param dc
+	 */
+	public static void addUnit(GoodsQueryModel gqm, DetachedCriteria dc) {
 		if(gqm.getUnit()!=null && gqm.getUnit().trim().length()>0){
 			dc.add(Restrictions.eq("unit", gqm.getUnit().trim()));
 		}
+	}
+
+	/**
+	 * Adding in-price to dc
+	 * @param gqm
+	 * @param dc
+	 */
+	public static void addInPrice(GoodsQueryModel gqm, DetachedCriteria dc) {
 		if(gqm.getInPrice()!=null && gqm.getInPrice()>0){
 			dc.add(Restrictions.ge("inPrice", gqm.getInPrice()));
 		}
+	}
+
+	/**
+	 * Adding in-price2 to dc
+	 * @param gqm
+	 * @param dc
+	 */
+	public static void addInPrice2(GoodsQueryModel gqm, DetachedCriteria dc) {
 		if(gqm.getInPrice2()!=null && gqm.getInPrice2()>0){
 			dc.add(Restrictions.le("inPrice", gqm.getInPrice2()));
 		}
+	}
+
+	/**
+	 * Adding out-price to dc
+	 * @param gqm
+	 * @param dc
+	 */
+	public static void addOutPrice(GoodsQueryModel gqm, DetachedCriteria dc) {
 		if(gqm.getOutPrice()!=null && gqm.getOutPrice()>0){
 			dc.add(Restrictions.ge("outPrice", gqm.getOutPrice()));
 		}
+	}
+
+	/**
+	 * Adding out-price2 to dc
+	 * @param gqm
+	 * @param dc
+	 */
+	public static void addOutPrice2(GoodsQueryModel gqm, DetachedCriteria dc) {
 		if(gqm.getOutPrice2()!=null && gqm.getOutPrice2()>0){
 			dc.add(Restrictions.le("outPrice", gqm.getOutPrice2()));
 		}
+	}
+
+	
+	
+	public void doQbc(DetachedCriteria dc,BaseQueryModel qm){
+		GoodsQueryModel gqm = (GoodsQueryModel) qm;
+		
+		addGtm(gqm, dc);
+		addName(gqm, dc);
+		addProducer(gqm, dc);
+		addOutPrice2(gqm, dc);
+		addOutPrice(gqm, dc);
+		addInPrice(gqm, dc);
+		addInPrice2(gqm, dc);
+		addUnit(gqm, dc);
+		
 	}
 
 	public List<GoodsModel> getAllByGtmUuid(Long gtmUuid) {
