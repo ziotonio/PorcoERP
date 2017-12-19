7a3ea9b57690c520d81cbc9fc4ed342dcb9ff06d
f.valrosso <f.valrosso@c81b5bb8-e782-4fe7-967c-ac36ce986653>
2017-12-19 15:48:25 +0000

    refs closes #9410 @2h30m

--- src/cn/itcast/invoice/invoice/supplier/dao/impl/SupplierDaoImpl.java
+++ src/cn/itcast/invoice/invoice/supplier/dao/impl/SupplierDaoImpl.java
@@ -15,22 +15,48 @@ public class SupplierDaoImpl extends BaseDaoImpl<SupplierModel> implements Suppl
 	public void doQbc(DetachedCriteria dc,BaseQueryModel qm){
 		SupplierQueryModel sqm = (SupplierQueryModel) qm;
 		
+		control1(sqm,dc);
+
+		control2(sqm,dc);
+
+		control3(sqm,dc);
+
+		control4(sqm,dc);
+		
+		
+	}
+
+	public void control1(SupplierQueryModel sqm,DetachedCriteria dc) {
 		if(sqm.getName()!=null && sqm.getName().trim().length()>0){
 			dc.add(Restrictions.like("name", "%"+sqm.getName().trim()+"%"));
 		}
+	}
+	
+	public void control2(SupplierQueryModel sqm,DetachedCriteria dc) {
 		if(sqm.getContact()!=null && sqm.getContact().trim().length()>0){
 			dc.add(Restrictions.like("contact", "%"+sqm.getContact().trim()+"%"));
 		}
+		
+	}
+	
+	public void control3(SupplierQueryModel sqm,DetachedCriteria dc) {
 		if(sqm.getTele()!=null && sqm.getTele().trim().length()>0){
 			dc.add(Restrictions.like("tele", "%"+sqm.getTele().trim()+"%"));
 		}
+		
+	}
+	
+	
+	public void control4(SupplierQueryModel sqm,DetachedCriteria dc) {
 		if(sqm.getNeeds()!=null && sqm.getNeeds()!= -1){
 			dc.add(Restrictions.eq("needs", sqm.getNeeds()));
 		}
+		
 	}
-
+	
+	
 	public List<SupplierModel> getAllUnion() {
-		//获取所有与商品类别具有关联数据的供应商信息
+		//èŽ·å�–æ‰€æœ‰ä¸Žå•†å“�ç±»åˆ«å…·æœ‰å…³è�”æ•°æ�®çš„ä¾›åº”å•†ä¿¡æ�¯
 		String hql = "select distinct sm from SupplierModel sm join sm.gtms";
 		return this.getHibernateTemplate().find(hql);
 	}
@@ -39,4 +65,5 @@ public class SupplierDaoImpl extends BaseDaoImpl<SupplierModel> implements Suppl
 		String hql = "select distinct sm from SupplierModel sm join sm.gtms gtm join gtm.gms";
 		return this.getHibernateTemplate().find(hql);
 	}
+	
 }
