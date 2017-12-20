7c3c65042a0b6deed7acc7f3b3274adc9e8dcb96
f.schiavone13 <f.schiavone13@c81b5bb8-e782-4fe7-967c-ac36ce986653>
2017-12-20 16:06:02 +0000

    refs closes #9415 @1h00m closes #9416 @1h00m

--- src/cn/itcast/invoice/util/listener/LoadResourceListener.java
+++ src/cn/itcast/invoice/util/listener/LoadResourceListener.java
@@ -1,5 +1,8 @@
 package cn.itcast.invoice.util.listener;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,6 +10,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -15,23 +21,42 @@ import javax.servlet.ServletContextListener;
 import cn.itcast.invoice.auth.res.business.ebi.ResEbi;
 
 public class LoadResourceListener implements ServletContextListener{
+	Properties props = new Properties();
+	FileInputStream in = null;
 	
+	/**
+	 * Creation of the contextDestroyed method for ServletContextListener Class
+	 */
 	public void contextDestroyed(ServletContextEvent sce) {
 	}
 
+	/**
+	 * Creation of the contextInitialized method for ServletContextListener Class
+	 */
 	public void contextInitialized(ServletContextEvent event) {
-		//服务器启动时，加载所有全资源数据:JDBC
+		//æœ�åŠ¡å™¨å�¯åŠ¨æ—¶ï¼ŒåŠ è½½æ‰€æœ‰å…¨èµ„æº�æ•°æ�®:JDBC
 		Connection conn = null;
 		try {
+			ArrayList<String> Info = getDBInfo();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		/**
+		 * Creation of connection to the ERP database 
+		 */
+		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/erpdb1","root","root");
+			conn = DriverManager.getConnection(getDBInfo().get(0), getDBInfo().get(1), getDBInfo().get(2));
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("select url from tbl_res");
 			List<String> temp = new ArrayList<String>();
 			while(rs.next()){
 				temp.add(rs.getString("url"));
 			}
-			//将集合放入ServletContext范围
+			//å°†é›†å�ˆæ”¾å…¥ServletContextèŒƒå›´
 			event.getServletContext().setAttribute("resAllUrl", temp);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -43,6 +68,30 @@ public class LoadResourceListener implements ServletContextListener{
 			}
 		}
 		
+	}
+	
+	
+	/**
+	 * This method connects to the file where db info are saved
+	 * @return
+	 * @throws IOException
+	 */
+	private ArrayList<String> getDBInfo() throws IOException {
+		ArrayList<String> DBInfo = new ArrayList();
+		try {
+			in = new FileInputStream("src/cn/itcast/invoice/util/listener/db.properties");
+			props.load(in);
+			DBInfo.add(props.getProperty("db.url"));
+			DBInfo.add(props.getProperty("db.user"));
+			DBInfo.add(props.getProperty("db.password"));
+		}catch(FileNotFoundException e) {
+			Logger lgr = Logger.getLogger(LoadResourceListener.class.getName());
+	        lgr.log(Level.SEVERE, e.getMessage(), e);
+		}
+		finally {
+			in.close();
+		}
+		return DBInfo;
 	}
 
 }
