e268ef08ca004f58d71fae8dbbb2562151552332
v.nigro8 <v.nigro8@c81b5bb8-e782-4fe7-967c-ac36ce986653>
2017-12-23 14:38:30 +0000

    refs closes #10304 @0h10m

--- WebRoot/js/Calendar.js
+++ WebRoot/js/Calendar.js
@@ -70,12 +70,13 @@ for(i = 0; i < 6; i++)
 str += '<tr Author="alin" align="center">';
 for(j = 0; j < 7; j++)
 {
-str += '<td Author="alin" class="dayOut" id="cdrDay'+(n++)+'" width="13%"></td>\n';
+str = str + '<td Author="alin" class="dayOut" id="cdrDay'+(n)+'" width="13%"></td>\n';
+n=n+1;
 }
-str += '</tr>';
+str = str + '</tr>';
 }
-str += '</table>\n';
-str += '<table Author="alin" class="cdrBodyBar" cellSpacing="2" cellPadding="0"><tr align="center" Author="alin"><td Author="alin" style="cursor:pointer;" onclick="'+this.Obj+'.getToday();">今天：'+new Date().toFormatString("yyyy年mm月dd日")+'</td></tr></table>\n';
+str = str + '</table>\n';
+str = str + '<table Author="alin" class="cdrBodyBar" cellSpacing="2" cellPadding="0"><tr align="center" Author="alin"><td Author="alin" style="cursor:pointer;" onclick="'+this.Obj+'.getToday();">今天：'+new Date().toFormatString("yyyy年mm月dd日")+'</td></tr></table>\n';
 return str;
 };
 Calendar.prototype.getBodyBar = function()
@@ -356,7 +357,7 @@ var _month = this.date.getMonth() + 1;
 var _date = this.date.getDate();
 if(_year > 999 && _year <10000)
 {
-if(isnext){_year++;}else{ _year --;}
+if(isnext){_year= _year + 1;}else{ _year = _year - 1;}
 }
 else
 {
@@ -369,11 +370,11 @@ Calendar.prototype.onChangeMonth = function(isnext)
 var _year = this.date.getFullYear();
 var _month = this.date.getMonth() + 1;
 var _date = this.date.getDate();
-if(isnext){ _month ++;} else {_month--;}
+if(isnext){ _month= _month + 1;} else {_month= _month - 1;}
 if(_year > 999 && _year <10000)
 { 
-if(_month < 1) {_month = 12; _year--;}
-if(_month > 12) {_month = 1; _year++;}
+if(_month < 1) {_month = 12; _year= _year -1;}
+if(_month > 12) {_month = 1; _year= year + 1;}
 }
 else
 {
