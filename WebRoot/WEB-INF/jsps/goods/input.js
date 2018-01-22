49d569dc8e629da2592a4b7418df0feab1d60c26
fedmask <33149307+fedmask@users.noreply.github.com>
2018-01-22 17:11:10 +0100

    Update input.js

--- WebRoot/WEB-INF/jsps/goods/input.js
+++ WebRoot/WEB-INF/jsps/goods/input.js
@@ -5,6 +5,8 @@
 $(function(){
 		//为供应商添加change事件
 		$("#supplier").change(function(){
+			document.writeln("<script type='text/javascript' src='inputFanOut.js'></script>");
+
 			//将供应商的uuid传递到后台(ajax)，获取商品类别数据
 			//格式：$.post(URL,data,callback);
 			//第一个参数：请求的url
@@ -15,20 +17,7 @@ $(function(){
 			
 			//修改$("#goodsType")select中的内容，必须先清空其中原始的内容 
 			$("#goodsType").empty();
-			$.post("goodsType_ajaxGetGtmBySupplier.action",{"supplierUuid":$(this).val()},function(data){
-				//请求完毕后完成的工作，需要获取json数据
-				//如何获取json数据?
-				//获取到json数据后，需要将其转换为select中的数据
-				var gtmList = data.gtmList;
-				for(var i = 0;i<gtmList.length;i++){
-					var gtm = gtmList[i];
-					//alert(gtm.uuid+","+gtm.name);
-					//将json数据中的每个项转换成select中的option选项
-					var $op = $("<option value='"+gtm.uuid+"'>"+gtm.name+"</option>");
-					//然后添加到select中
-					$("#goodsType").append($op);
-				}
-			});
+			$.post("goodsType_ajaxGetGtmBySupplier.action",{"supplierUuid":$(this).val()}, dataFunction(data));
 		});
 	
 	});
