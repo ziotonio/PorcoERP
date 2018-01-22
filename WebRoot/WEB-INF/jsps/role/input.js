4ce01576847cd4165d3e1b73c7e1d2bd821e2217
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-01-22 17:37:06 +0100

    Update input.js

--- WebRoot/WEB-INF/jsps/role/input.js
+++ WebRoot/WEB-INF/jsps/role/input.js
@@ -13,19 +13,15 @@ $(function() {
             });
 		});
 		*/
+	document.writeln("<script type='text/javascript' src='inputFanOut.js'></script>");
+
 		
-		$("#all").click(function(){
-			//全选
-			//将所有的全部选中
-			$("[name='resUuids']").attr("checked",$(this).attr("checked")=="checked");
-		});
+		$("#all").click(checked2());
 		$("#reverse").click(function(){
+
 			//反选
 			//将所有选项的状态进行切换
-			$("[name='resUuids']").each(function(){
-				//将状态切换
-				$(this).attr("checked",$(this).attr("checked")!="checked");
-			});
+			$("[name='resUuids']").each(checkedFun());
 		});
 		
 	});
