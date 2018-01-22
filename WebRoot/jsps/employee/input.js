d0a16cba6e8e72db33970310070fc53ba2f43fc3
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-01-22 17:52:47 +0100

    Update input.js

--- WebRoot/jsps/employee/input.js
+++ WebRoot/jsps/employee/input.js
@@ -3,13 +3,10 @@
  * @returns
  */
 $(function() {
+	document.writeln("<script type='text/javascript' src='inputFanOut.js'></script>");
+
 		$("#all").click(function() {
 			$("[name=roles]:checkbox").attr("checked",$("#all").attr("checked")=="checked");
 		});
-		$("#reverse").click(function() {
-			$("[name=roles]:checkbox").each(function () {
-                $(this).attr("checked", !$(this).attr("checked"));
-            });
-
-		});
+		$("#reverse").click(checkBox());
 });
