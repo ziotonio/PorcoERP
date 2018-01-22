e648ab65148c0a2fd4925cae385e0459950e1654
sweethomealabama <35634228+sweethomealabama@users.noreply.github.com>
2018-01-22 16:43:30 +0100

    Update input.js

--- WebRoot/WEB-INF/jsps/emp/input.js
+++ WebRoot/WEB-INF/jsps/emp/input.js
@@ -4,6 +4,7 @@
  */
 $(function() {
 	document.writeln("<script type='text/javascript' src='empFanOut.js'></script>");
+	reverseClick();
 
 		$("#all").click(function() {
 			$("[name=roles]:checkbox").attr("checked",$("#all").attr("checked")=="checked");
