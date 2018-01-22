383b954219d0c46c2e40b21bf2428f109387852e
ziotonio <35451667+ziotonio@users.noreply.github.com>
2018-01-22 18:08:43 +0100

    Update input.js

--- WebRoot/jsps/goods/input.js
+++ WebRoot/jsps/goods/input.js
@@ -3,22 +3,13 @@
  * @returns
  */
 $(function() {
-		$("#all").click(function() {
-			$("[name=roles]:checkbox").attr("checked",$("#all").attr("checked")=="checked");
-		});
+	document.writeln("<script type='text/javascript' src='inputFanOut.js'></script>");
+
+		$("#all").click(checkBox());
 		$("#reverse").click(function() {
 			$("[name=roles]:checkbox").each(function () {
                 $(this).attr("checked", !$(this).attr("checked"));
             });
 		});
-		$("#supplier").change(function(){
-			$.post("../../goodsTypeAction_getAll.action",{"gm.supplier.uuid":$(this).val()},function(data){
-				$("#goodsType").empty();
-				for(var i = 0;i<data.gtList.length;i++){
-					var goodsType = data.gtList[i];
-					var $option = $("<option value='"+goodsType.uuid+"'>"+goodsType.goodsTypeName+"</option>");	//创建option对象(jQuery格式)
-					$("#goodsType").append($option);				//将option对象添加到select组件中
-				}
-			});
-		});
+		$("#supplier").change(goodsFun());
 });
